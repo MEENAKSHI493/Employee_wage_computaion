ca55b7e8480e532937fded216eb8fc336fe3003f
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-03 17:27:09 +0530

    Wellcome employee

--- com/Bridgelabz/Employeewage.java
+++ com/Bridgelabz/Employeewage.java
@@ -3,8 +3,7 @@ package com.Bridgelabz;
 public class Employeewage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to bridze lab");
+		System.out.println("Welcome to Bridgelabe employee");
 
 	}
 
