df8dc6c103ae07330fbe959fb3ce8c5098441cd6
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-03 17:43:17 +0530

    Employee Present or Absent

--- com/Bridgelabz/Employeewage.java
+++ com/Bridgelabz/Employeewage.java
@@ -3,8 +3,12 @@ package com.Bridgelabz;
 public class Employeewage {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Bridgelabe employee");
-
+		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		if (empCheck == 1) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
 	}
 
 }
