4f1bf0bac48c4e4f2a8f7b0c7a384ac2aede62cf
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-04 00:19:54 +0530

    Write Class Method  To Caluclate Employee Wage

--- com/Bridgelabz/Employeewage.java
+++ com/Bridgelabz/Employeewage.java
@@ -2,43 +2,45 @@ package com.Bridgelabz;
 
 public class Employeewage {
 
-	public static void calculateWages(int FullDayHr, int PartTimeHr, int WagePerHr, String companyName) {
-		int day = 0;
-		int empHr = 0;
+	public static void calculateWages() 
+	{
+		int FullDayHr=8;
+		int PartTimeHr=4;
+		int WagePerHr=20;
+		int day=0;
+		int empHr=0;
 		int wage = 0;
 		while (day <= 19) {
 			int n = (int) (Math.random() * 10) % 3;
-			switch (n) {
-			case 1:
-//System.out.println("present fullday");
-				wage = (wage + (WagePerHr * FullDayHr));
-				empHr = (empHr + FullDayHr);
-				day++;
+		switch (n) {
+		case 1:
+			System.out.println("present fullday");
+			wage = (wage + (WagePerHr * FullDayHr));
+            empHr=(empHr+ FullDayHr);
+			day++;
+			break;
+		case 2:
+			System.out.println("present PartTime");
+			wage = (wage + (WagePerHr * PartTimeHr));
+			empHr=(empHr+ PartTimeHr);
+			day++;
+			break;
+		default:
+			System.out.println("absent");
+		    day++;
+			if( empHr == 100)
 				break;
-			case 2:
-//System.out.println("present PartTime");
-				wage = (wage + (WagePerHr * PartTimeHr));
-				empHr = (empHr + PartTimeHr);
-				day++;
-				break;
-			default:
-//System.out.println("absent");
-				day++;
-				if (empHr == 100)
-					break;
-			}
 		}
-		System.out.println("Calculate Wages For :" + companyName);
-		System.out.println("EmpHr :" + empHr);
-		System.out.println("Day :" + day);
-		System.out.println("Total Wage is :" + wage);
-		System.out.println(" ");
+		 }
+		System.out.println("total wage of a month is");
+		System.out.println(wage);
+		System.out.println(empHr);
+		System.out.println(day);
 	}
-
-	public static void main(String[] args) {
-		calculateWages(8, 2, 20, "Dmart");
-		calculateWages(8, 4, 20, "Reliance Digital");
-		calculateWages(8, 6, 20, "Patrol Pump");
+	
+	public static void main(String[] args) 
+	{
+		calculateWages();
 	}
 
 }
