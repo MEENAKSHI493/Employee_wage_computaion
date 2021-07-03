619f49f888f7dc6697d4c9fd9372b2c40f3dc4eb
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-03 23:00:19 +0530

    Employee partime time dailywage

--- com/Bridgelabz/Employeewage.java
+++ com/Bridgelabz/Employeewage.java
@@ -7,29 +7,27 @@ public class Employeewage {
 		int PartTimeHours = 4;
 		int FullDay = 8;
 		int WageParHour = 20;
-		final int empPresent = 1;
-		final int empPartTime = 2;
-		int dailyWage = 0;
-
+		int empPresent = 1;
+		int empPartTime = 2;
 		int empCheck = (int)Math.floor(Math.random() * 10) % 3;
-		
-		switch (empCheck) {
-		case empPresent:
-			dailyWage = FullDay*WageParHour;
+		if (empCheck == empPresent) 
+		{
+			int dailyWage = FullDay*WageParHour;
 			System.out.println("Employee is Present");
 			System.out.println("Employee Daily Wage Is : "+ dailyWage);
-			break;
-		case empPartTime:{
-			dailyWage = PartTimeHours*WageParHour;
+		}
+		else if(empCheck == empPartTime)
+		{
+			int dailyWage = PartTimeHours*WageParHour;
 			System.out.println("Employee Part Time is Present");
 			System.out.println("Employee Part Time Wage Is : "+ dailyWage);
-			break;
 		}
-		default:
+		else 
+		{
 			System.out.println("Employee is Absent");
-			break;
 		}
 	}
+
 
 	}
     
